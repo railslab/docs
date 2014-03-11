d9cbe4925136b896416023f8cb3d5b0612d42824
Marcos Neves <marcos.neves@gmail.com>
2014-03-11 19:45:36 -0300

    comentarios de instalacao

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -5,23 +5,32 @@ sudo visudo -f /etc/sudoers.d/neves-passwordless
 sudo apt-get update
 sudo apt-get -y install git build-essential sqlite3 libsqlite3-dev wget curl zsh libssl-dev exfat-utils openssh-server
 
+# zsh shell
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 chsh -s /bin/zsh neves
+# sair do shell e voltar
 
-# rbenv + ruby + rails
+# Ambiente ruby/rails: rbenv/ruby/rails
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+
+PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
 rbenv install 2.1.1 -v
 rbenv global 2.1.1
+
 gem update --system
 
 echo "gem: --no-ri --no-rdoc -V" > ~/.gemrc
 gem update --system
 gem install bundler
+
 cat <<'EOF' > Gemfile
 source 'https://rubygems.org'
 gem "rails"
